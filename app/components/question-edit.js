888282a04da67f62cc0194ccf1923a365cb4c189
rouz majlessi <rmajlessi@gmail.com>
2016-10-29 23:11:38 -0700

    add form to edit question and refactor and add model to quesiton-tile

--- app/components/question-edit.js
+++ app/components/question-edit.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editQuestionShow: false,
+  actions: {
+    editQuestionShow() {
+      this.set('editQuestionShow', true);
+    },
+  }
 });
