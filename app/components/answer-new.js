f99ce1928e5d6c71270f2468d5ad906fd9e81478
rouz majlessi <rmajlessi@gmail.com>
2016-10-30 00:44:29 -0700

    can now save ansers to database/firebase

--- app/components/answer-new.js
+++ app/components/answer-new.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+
+    save4() {
+      var params = {
+        name: this.get("name"),
+        answers: this.get("answers"),
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('save5', params);
+    }
+  }
 });
--- app/routes/question-display.js
+++ app/routes/question-display.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
+  },
+
+  actions: {
+    save6(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question-display', params.question);
+    }
   }
 });
