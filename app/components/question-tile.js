f09f5ba31467a51fbf34aae59979ba548d9e9804
rouz majlessi <rmajlessi@gmail.com>
2016-10-29 23:59:48 -0700

    import json file with answers too firebase

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editQuestionForm: false,
+  actions: {
+    update(question, params) {
+      this.sendAction('update', question, params);
+    }
+  }
 });
