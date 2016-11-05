fecdb193d21854f5a8cbdaf10dce64461058f2df
rouz majlessi <rmajlessi@gmail.com>
2016-11-05 14:47:18 -0700

    add loop to helper file for awaiting an answer to question that has none also add a for asking more answers to 3 or more

--- app/helpers/popular.js
+++ app/helpers/popular.js
@@ -1,7 +1,13 @@
+
 import Ember from 'ember';
 
 export function popular(params/*, hash*/) {
-  return params;
+  var answer = params[0];
+  if (answer.get('answers').get('length') === 0) {
+    return Ember.String.htmlSafe('<span class="zero-response">(awaiting response)</span>');
+  } else if (answer.get('answers').get('length') <= 3) {
+    return Ember.String.htmlSafe('<span class="some-response">(looking for a few more responses)</span>');
+  }
 }
 
 export default Ember.Helper.helper(popular);
