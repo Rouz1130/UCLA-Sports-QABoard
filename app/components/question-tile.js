b00a66cc435c782d18c0bc427dc1c08506004189
rouz majlessi <rmajlessi@gmail.com>
2016-11-05 15:04:18 -0700

    add faves function to question display component and add inject service

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favorite: Ember.inject.service(),
   editQuestionForm: false,
   actions: {
+    addToList(fave) {
+      this.get('favorite').add(fave);
+    },
+
     update(question, params) {
       this.sendAction('update', question, params);
     }
