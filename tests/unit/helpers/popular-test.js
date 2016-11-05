fcbb27e944399cf0b8bfd59d116f64e377f31a20
rouz majlessi <rmajlessi@gmail.com>
2016-11-05 14:39:23 -0700

    add helper file

---
+++ tests/unit/helpers/popular-test.js
@@ -0,0 +1,12 @@
+
+import { popular } from 'fourm-board/helpers/popular';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | popular');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = popular([42]);
+  assert.ok(result);
+});
+
